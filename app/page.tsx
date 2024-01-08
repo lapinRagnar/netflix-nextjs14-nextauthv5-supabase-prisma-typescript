001c5f8d93400eda9ba73d4b40c6a01a36440dd0
lapinRagnar <lapinragnar@gmail.com>
2024-01-08 20:32:01 +0100

    install Button de shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
       bonjour
+      <div>
+        <Button>Cliquer</Button>
+      </div>
     </main>
 
   )
