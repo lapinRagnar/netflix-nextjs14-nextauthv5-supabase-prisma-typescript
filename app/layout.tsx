0127da5542f343043c66c109ab61baec405139e7
lapinRagnar <lapinragnar@gmail.com>
2024-01-09 17:07:06 +0100

    la navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { NextAuthProvider } from '@/components/NextAuthProvider'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,8 +21,13 @@ export default function RootLayout({
       <body className={inter.className}>
         
         <NextAuthProvider>
-          
-          {children}
+          <div>
+            <Navbar />
+          </div>
+
+          <div>
+            {children}
+          </div>
         
         </NextAuthProvider>  
       
