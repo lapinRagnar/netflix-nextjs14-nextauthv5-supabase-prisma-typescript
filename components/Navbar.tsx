9e366df2653ba00f2d0a849255d0fbd698622aff
lapinRagnar <lapinragnar@gmail.com>
2024-01-11 08:45:36 +0100

    fix padding navbar for mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
 
   return (
-    <div className="w-full max-w-7xl mx-auto items-center justify-between x-5 sm:px-6 py-5 lg:px-8 flex">
+    <div className="w-full max-w-7xl mx-auto items-center justify-between px-5 sm:px-6 py-5 lg:px-8 flex">
       <div className="flex items-center">
         <Link href={'/home'} className="w-32">
           <Image 
