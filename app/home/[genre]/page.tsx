3f9fd580606c67b11b69ec2df5ff9070bbd2daed
lapinRagnar <lapinragnar@gmail.com>
2024-01-11 08:44:18 +0100

    suite - afficher la card movie pour chaque categorie

--- app/home/[genre]/page.tsx
+++ app/home/[genre]/page.tsx
@@ -1,5 +1,7 @@
 import { auth } from "@/auth";
+import MovieCard from "@/components/MovieCard";
 import { db } from "@/lib/db";
+import Image from "next/image";
 
 const getData = async (category: string, userId: string) => {
 
@@ -105,11 +107,43 @@ const CategoryPage = async ({ params }: { params: { genre: string }}) => {
 
 
   return (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-5 sm:px-0 mt-10 gap-6">
       { data && data.map((movie) => (
-        <div key={movie.id}>
-          <h1>{movie.title}</h1>
-          <p>{movie.overview}</p>
+        <div key={movie.id} className="relative h-60">
+          <Image 
+            src={movie.imageString}
+            alt="Movie"
+            width={500}
+            height={400}
+            className="rounded-sm absolute w-full h-full object-cover"
+          />
+
+          {/* effet hover sur l'image */}
+          <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+            <div className="bg-gradient-to-b from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex items-center justify-center ">
+              <Image 
+                src={movie.imageString}
+                alt="Movie"
+                width={800}
+                height={800}
+                className="rounded-lg absolute w-full h-full object-cover -z-10"
+              />
+
+              <MovieCard 
+                key={movie.id}
+                age={movie.age}
+                movieId={movie.id}
+                overview={movie.overview}
+                time={movie.duration}
+                title={movie.title}
+                watchListId={movie.WatchLists[0]?.id}
+                watchList={movie.WatchLists.length > 0 ? true : false}
+                year={movie.release}
+                youtubeUrl={movie.youtubeString}
+              />
+            </div>
+          </div>
+
         </div>
       ))}
     </div>
